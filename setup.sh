cc9bdf6cc23c6e12cf40fff9fa22f0dbea70ac06
Collin Arnett <collin@arnett.it>
2020-03-25 04:24:57 +0000

    check OS and then install requirements

--- setup.sh
+++ setup.sh
@@ -1,14 +1,33 @@
-#!/bin/bash
+#!/usr/bin/env bash
 git clone https://github.com/collinarnett/dev-environment.git
-cp .zshrc .vimrc .zsh_plugins.txt ~
-$TERM_PATH=~/.config/terminator
-mkdir $TERM_PATH
-cp -r plugins $TERM_PATH
-cp config $TERM_PATH
-sudo apt install -y zsh fonts-powerline terminator
-chsh -s $(which zsh)
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-curl -sfL git.io/antibody | sudo sh -s - -b /usr/local/bin
-sudo apt install python3-pip
-pip3 installl pylint pyls flake8
-source .zshrc
+cd dev-environment
+
+# OSX
+if [ "$(uname)" == "Darwin" ]; then
+    echo "Setting up config files..."
+    cp .zshrc .vimrc .zsh_plugins.txt ~
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"    
+    brew cask install iterm2
+    brew install zsh fonts-powerline
+    chsh -s $(which zsh)
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    curl -sfL git.io/antibody | sudo sh -s - -b /usr/local/bin
+
+# Linux
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    echo "Setting up config files..."
+    cp .zshrc .vimrc .zsh_plugins.txt ~    
+    echo "Setting up terminator..."
+    $TERM_PATH=~/.config/terminator
+    mkdir $TERM_PATH
+    cp -r plugins $TERM_PATH
+    cp config $TERM_PATH
+    echo "Setting up essential apps..."
+    sudo apt install -y zsh fonts-powerline terminator python3-pip
+    chsh -s $(which zsh)
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    curl -sfL git.io/antibody | sudo sh -s - -b /usr/local/bin
+    pip3 install pylint pyls flake8
+fi
+
+npm install eslint
