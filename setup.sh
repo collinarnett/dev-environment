fb868a11b28e919794eed36c6f93aa904d2894a0
Collin Arnett <collin@arnett.it>
2020-04-15 00:39:41 +0000

    added linters and fixers for ALE

--- setup.sh
+++ setup.sh
@@ -7,11 +7,12 @@ if [ "$(uname)" == "Darwin" ]; then
     echo "Setting up config files..."
     cp .zshrc .vimrc .zsh_plugins.txt ~
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"    
-    brew cask install iterm2
-    brew install zsh fonts-powerline
+    brew cask install iterm2 
+    brew install zsh fonts-powerline python3 python3-pip
     chsh -s $(which zsh)
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
     curl -sfL git.io/antibody | sudo sh -s - -b /usr/local/bin
+    pip3 install pylint pyls flake8 vint
 
 # Linux
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
@@ -23,7 +24,7 @@ elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     cp -r plugins $TERM_PATH
     cp config $TERM_PATH
     echo "Setting up essential apps..."
-    sudo apt install -y zsh fonts-powerline terminator python3-pip
+    sudo apt install -y zsh fonts-powerline terminator python3 python3-pip
     chsh -s $(which zsh)
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
     curl -sfL git.io/antibody | sudo sh -s - -b /usr/local/bin
